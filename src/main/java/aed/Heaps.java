dc3cf2eafbf74b5f3ae352b038def8bf8c1a0e4b
misProyectosPropios <inakifrutos@outlook.com>
2024-11-02 16:15:18 +0100

    Implemented encolar

--- src/main/java/aed/Heaps.java
+++ src/main/java/aed/Heaps.java
@@ -18,6 +18,41 @@ public class Heaps<T> {
 
     public void Encolar(T element) {
         //Needs to be implemented
+        //Adds element to the last position
+        this.heap.add(element);
+        this.subirElemento();
+    }
+
+    private void subirElemento() {
+        int position = this.heap.size() - 1;
+        int positionParent = calcularPosicionPadre(position);
+        while (position != 0 && prioridadMayorQuePadre(position, positionParent)) {
+            swap(position, positionParent);
+            position = positionParent;
+            positionParent = calcularPosicionPadre(position);
+        }
+    }
+
+    private int calcularPosicionPadre(int position) {
+        int positionParent;
+        if (position % 2 == 0) {
+            positionParent = (position - 2) / 2;
+        } else {
+            positionParent = (position - 1) / 2;
+        }
+        return positionParent;
+    }
+
+    //Implementarlo
+    private boolean prioridadMayorQuePadre(int positionElement, int positionParentElement) {
+        return true;
+    }
+
+    private void swap(int position1, int position2) {
+        T guardar = this.heap.get(position1);
+        this.heap.set(position1, this.heap.get(position2));
+        this.heap.set(position2, guardar);
+
     }
 
     public T desencolar() throws Exception {
@@ -27,6 +62,6 @@ public class Heaps<T> {
         return this.heap.get(0);
     }
 
-    
+
 
 }
