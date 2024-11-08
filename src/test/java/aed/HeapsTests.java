57967179192d74aa4a9f69f254935df3dbe74ac6
misProyectosPropios <inakifrutos@outlook.com>
2024-11-08 14:10:05 -0300

    Create HeapsTests.java

---
+++ src/test/java/aed/HeapsTests.java
@@ -0,0 +1,24 @@
+package aed;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+
+public class HeapsTest {
+    Comparator<Ciudad> comparatorGananciaNeta = Comparator.comparing(Ciudad::gananciaNeta); //hace falta el getter de ese tipo 
+
+    //Comparator<Traslado> comparator = Comparator.comparingInt(Traslado::gananciaNeta);
+    //Heaps<Integer> heap = new Heaps<>();
+
+    @BeforeEach
+    void init() {
+         
+    }
+
+}
