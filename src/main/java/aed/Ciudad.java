901942854247bc9cf0a48924ef721c416d6c1fa9
misProyectosPropios <inakifrutos@outlook.com>
2024-11-08 12:01:40 -0300

    Update Ciudad.java

---
+++ src/main/java/aed/Ciudad.java
@@ -0,0 +1,35 @@
+package aed;
+
+public class Ciudad<T> {
+    int nombre;
+    int gananciaNeta;
+    int perdidaNeta;
+    int superavit;
+
+
+    public Ciudad(int posicion){
+    this.nombre=posicion;
+    this.gananciaNeta=0;
+    this.perdidaNeta=0;
+    this.superavit=gananciaNeta-perdidaNeta;
+}
+
+   //Capaz hay un nombre para estos metodos que los haga menos confuso?
+    public void compro(int gasto){
+        this.perdidaNeta += gasto;
+        this.superavit = gananciaNeta - perdidaNeta;
+    }
+    
+    public void vendio(int ganancia){
+        this.gananciaNeta += ganancia;
+        this.superavit = gananciaNeta - perdidaNeta;
+    }
+
+    public int gananciaNeta() {
+        return this.gananciaNeta;
+    }
+    public int superavit(){
+        return this.superavit;
+    }
+
+}
