e8fb458d22a8b24e9b4cb29cb946fa800e92f2f5
misProyectosPropios <inakifrutos@outlook.com>
2024-11-08 14:10:51 -0300

    Update Traslado.java

--- src/main/java/aed/Traslado.java
+++ src/main/java/aed/Traslado.java
@@ -1,12 +1,16 @@
 package aed;
 
-public class Traslado {
+import java.util.Comparator;
+
+public class Traslado{
     
     int id;
     int origen;
     int destino;
     int gananciaNeta;
     int timestamp;
+    int indiceAHeapGanancia;
+    int indiceAHeapAntiguedad;
 
     public Traslado(int id, int origen, int destino, int gananciaNeta, int timestamp){
         this.id = id;
@@ -14,5 +18,12 @@ public class Traslado {
         this.destino = destino;
         this.gananciaNeta = gananciaNeta;
         this.timestamp = timestamp;
+        this.indiceAHeapGanancia = 0;
+        this.indiceAHeapAntiguedad = 0;
+        
     }
+
+    public int gananciaNeta() { return this.gananciaNeta;}
+
+    public int timestamp() {return this.timestamp; }
 }
