383d652f69a0ca337a537fd22302088122b852a5
misProyectosPropios <inakifrutos@outlook.com>
2024-11-08 14:11:07 -0300

    Update Ciudad.java

--- src/main/java/aed/Ciudad.java
+++ src/main/java/aed/Ciudad.java
@@ -1,27 +1,37 @@
 package aed;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 
-
-
-public class Ciudad<T>{
+public class Ciudad<T> {
     int nombre;
     int gananciaNeta;
     int perdidaNeta;
-    int superavit=gananciaNeta-perdidaNeta;
+    int superavit;
 
 
     public Ciudad(int posicion){
     this.nombre=posicion;
     this.gananciaNeta=0;
-    this.perdidaNeta=0;}
+    this.perdidaNeta=0;
+    this.superavit=gananciaNeta-perdidaNeta;
+}
 
    //Capaz hay un nombre para estos metodos que los haga menos confuso?
     public void compro(int gasto){
-        this.perdidaNeta+=gasto;}
+        this.perdidaNeta += gasto;
+        this.superavit = gananciaNeta - perdidaNeta;
+    }
     
     public void vendio(int ganancia){
-        this.gananciaNeta+=ganancia;
+        this.gananciaNeta += ganancia;
+        this.superavit = gananciaNeta - perdidaNeta;
     }
+
+    public int gananciaNeta() {
+        return this.gananciaNeta;
+    }
+    public int superavit(){
+        return this.superavit;
+    }
+
+    public int nombre() { return this.nombre; }
 
 }
