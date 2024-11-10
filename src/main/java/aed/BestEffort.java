f4b3abf61ca697e8264b9270eab34c70cfbc393e
nachoFlow4k <165933862+nachoFlow4k@users.noreply.github.com>
2024-11-10 19:29:32 -0300

    Implemented nuevoSistema

--- src/main/java/aed/BestEffort.java
+++ src/main/java/aed/BestEffort.java
@@ -3,10 +3,52 @@ package aed;
 import java.util.ArrayList;
 
 public class BestEffort {
-    //Completar atributos privados
+    Heaps trasladosPorGanancia;
+    Heaps trasladosPorAntiguedad;
+    Heaps mayorSuperavit;
+    Ciudad[] ciudadesTotales;
+    int totalDespachados;
+    int sumaDeGananciaDeDespachos;
+    ArrayList<Integer> mayorGanancia;
+    ArrayList<Integer> mayorPerdida;
 
-    public BestEffort(int cantCiudades, Traslado[] traslados){
+
+    public BestEffort nuevoSistema(int cantCiudades, Traslado[] traslados){
         // Implementar
+        BestEffort res = new BestEffort () ;
+        
+        int index = 0 ;                                                                    
+	    
+        res.mayorPerdida = new ArrayList<Integer>();					           
+	    
+        res.mayorGanancia = new ArrayList<Integer>() ; 
+        
+        this.ciudadesTotales= new Ciudad[cantCiudades];                                          
+	    
+        while ( index < cantCiudades )    {                                                 
+		    Ciudad añadirCiudad = new Ciudad (index) ; 
+            res.mayorSuperavit.add(añadirCiudad) ;			           	
+ 		    res.ciudadesTotales[index]= añadirCiudad;                 	                
+		    res.mayorPerdida.add(añadirCiudad.nombre) ;                              	
+		    res.mayorGanancia.add(añadirCiudad.nombre) ;                             	
+		    index ++ ;                         
+        } 
+        
+        index = 0 ;                                                                       
+	    while ( index < traslados.length ){
+		Traslado trasladoEnCuestion = traslados[ index ] ;                      	   	
+		Traslado añadirTranslados = new Traslado ( trasladoEnCuestion.id , trasladoEnCuestion.origen , trasladoEnCuestion.destino , trasladoEnCuestion.gananciaNeta,trasladoEnCuestion.timestamp);
+		res.trasladosPorGanancia.add( añadirTranslados ) ;                         	
+		res.trasladosPorAntiguedad.add( añadirTranslados ) ;	                   	
+		index ++ ;   }                                                              	
+
+	    res.totalDespachados = 0 ;						           
+
+	    res.mayorSuperavit.FloydAlgorithm();						   
+	    res.trasladosPorGanancia.FloydAlgorithm() ;					   
+	    res.trasladosPorAntiguedad.FloydAlgorithm();					   
+        return res;				
+
     }
 
     public void registrarTraslados(Traslado[] traslados){
