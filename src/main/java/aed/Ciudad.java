5889b9aabdde5db934efad32c36daa609dc5ce4f
Fran Pucciarelli <franciscopucciarelli99@gmail.com>
2024-11-09 23:58:50 -0300

    En la clase Ciudad: cambié algunos nombres de variables/métodos (para hacerlos mas declarativos) e hice privados los atributos (sino no tiene sentido tener métodos getter y setter).

--- src/main/java/aed/Ciudad.java
+++ src/main/java/aed/Ciudad.java
@@ -1,35 +1,47 @@
 package aed;
 
 public class Ciudad<T> {
-    int nombre;
-    int gananciaNeta;
-    int perdidaNeta;
-    int superavit;
 
+    // Atributos privados de la clase.
+    private int nombre ;
+    private int gananciaNeta ;
+    private int perdidaNeta ;
+    private int superavit ;
 
-    public Ciudad(int posicion){
-    this.nombre=posicion;
-    this.gananciaNeta=0;
-    this.perdidaNeta=0;
-    this.superavit=gananciaNeta-perdidaNeta;
-}
+    // Constructor.
+    public Ciudad (int nombre_index) {
 
-   //Capaz hay un nombre para estos metodos que los haga menos confuso?
-    public void compro(int gasto){
-        this.perdidaNeta += gasto;
-        this.superavit = gananciaNeta - perdidaNeta;
+        this.nombre = nombre_index ;
+        this.gananciaNeta = 0 ;
+        this.perdidaNeta = 0 ;
+        this.superavit = gananciaNeta - perdidaNeta ;
+
+    }
+
+    public void aumentarPerdida (int monto) {
+
+        this.perdidaNeta += monto ;
+        this.superavit = gananciaNeta - perdidaNeta ;
+
     }
     
-    public void vendio(int ganancia){
-        this.gananciaNeta += ganancia;
-        this.superavit = gananciaNeta - perdidaNeta;
+    public void aumentarGanancia (int monto) {
+
+        this.gananciaNeta += monto ;
+        this.superavit = gananciaNeta - perdidaNeta ;
+
     }
 
-    public int gananciaNeta() {
-        return this.gananciaNeta;
+    public int obtenerGananciaNeta() {
+
+        return this.gananciaNeta ;
+
     }
-    public int superavit(){
-        return this.superavit;
+
+    public int obtenerSuperavit() {
+
+        return this.superavit ;
+        
     }
 
     public int nombre() { return this.nombre; }
