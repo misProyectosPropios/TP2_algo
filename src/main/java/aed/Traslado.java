7cdb7b9b5328893333e45554d90fda9be2c750a4
Fran Pucciarelli <franciscopucciarelli99@gmail.com>
2024-11-10 00:08:42 -0300

    En la clase Traslado: modifiqué el constructor. Por cómo tenemos planeado implementar el proc 'nuevoSistema()', los atributos 'indexAHeapGanancia' e 'indexAHeapAntiguedad' se pasan como parámetro.

--- src/main/java/aed/Traslado.java
+++ src/main/java/aed/Traslado.java
@@ -2,26 +2,28 @@ package aed;
 
 import java.util.Comparator;
 
-public class Traslado{
+public class Traslado {
     
-    int id;
-    int origen;
-    int destino;
-    int gananciaNeta;
-    int timestamp;
-    int indiceAHeapGanancia;
-    int indiceAHeapAntiguedad;
+    int id ;
+    int origen ;
+    int destino ;
+    int gananciaNeta ;
+    int timestamp ;
+    int indiceAHeapGanancia ;
+    int indiceAHeapAntiguedad ;
 
-    public Traslado(int id, int origen, int destino, int gananciaNeta, int timestamp){
-        this.id = id;
-        this.origen = origen;
-        this.destino = destino;
-        this.gananciaNeta = gananciaNeta;
-        this.timestamp = timestamp;
-        this.indiceAHeapGanancia = 0;
-        this.indiceAHeapAntiguedad = 0;
+    // Constructor.
+    public Traslado(int id, int origen, int destino, int gananciaNeta, int timestamp, int indexAHeapGanancia, int indexAHeapAntiguedad) {
+
+        this.id = id ;
+        this.origen = origen ;
+        this.destino = destino ;
+        this.gananciaNeta = gananciaNeta ;
+        this.timestamp = timestamp ;
+        this.indiceAHeapGanancia = indexAHeapGanancia ;
+        this.indiceAHeapAntiguedad = indexAHeapAntiguedad ;
         
-    }
+    } 
 
     public int gananciaNeta() { return this.gananciaNeta;}
 
