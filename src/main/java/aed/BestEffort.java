b119d54151749ebf56562bfde04e1e8df511a2d5
Fran Pucciarelli <166013213+franpuch@users.noreply.github.com>
2024-11-11 17:37:52 -0300

    Update BestEffort.java

--- src/main/java/aed/BestEffort.java
+++ src/main/java/aed/BestEffort.java
@@ -1,59 +1,98 @@
 package aed;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class BestEffort {
-    Heaps trasladosPorGanancia;
-    Heaps trasladosPorAntiguedad;
-    Heaps mayorSuperavit;
-    Ciudad[] ciudadesTotales;
-    int totalDespachados;
-    int sumaDeGananciaDeDespachos;
-    ArrayList<Integer> mayorGanancia;
-    ArrayList<Integer> mayorPerdida;
+
+    private Heaps<Traslado> trasladosPorGanancia;
+    private Heaps<Traslado> trasladosPorAntiguedad;
+    private Heaps<Ciudad> mayorSuperavit;
+    private Ciudad[] ciudadesTotales;
+    private int totalDespachados;
+    private int sumaDeGananciaDeDespachos;
+    private ArrayList<Integer> mayorGanancia;
+    private ArrayList<Integer> mayorPerdida;
 
 
 
 
-    public BestEffort (int cantCiudades, Traslado[] traslados){
-        // Implementar
-        //BestEffort res = new BestEffort () ;
-        /*
-        int index = 0 ;                                                                    
+    public BestEffort (int cantCiudades, Traslado[] traslados) {                                                                    
 	    
-        this.mayorPerdida = new ArrayList<Integer>();					           
+        // Creo las variables auxiliares (que me van a ayudar a llenar los atributos).
+        this.mayorPerdida = new ArrayList<Integer>() ;					           
 	    
         this.mayorGanancia = new ArrayList<Integer>() ; 
+
+        Ciudad[] mayorSuperavitAux = new Ciudad[cantCiudades] ;
+
+        Traslado[] trasladosPorGananciaAux = new Traslado[traslados.length] ;
+
+        Traslado[] trasladosPorAntiguedadAux = new Traslado[traslados.length] ;
         
-        this.ciudadesTotales= new Ciudad[cantCiudades];                                          
+        this.ciudadesTotales= new Ciudad[cantCiudades] ;                                          
 	    
-        while ( index < cantCiudades )    {                                                 
+        // Llenamos las variables auxiliares y algunos atributos.
+        int index = 0 ;
+        while ( index < cantCiudades ) {
+
 		    Ciudad añadirCiudad = new Ciudad (index) ; 
-            //res.mayorSuperavit.add(añadirCiudad) ;			           	
- 		    this.ciudadesTotales[index]= añadirCiudad;                 	                
-		    this.mayorPerdida.add(añadirCiudad.nombre) ;                              	
-		    this.mayorGanancia.add(añadirCiudad.nombre) ;                             	
-		    index ++ ;                         
+            mayorSuperavitAux[index] = añadirCiudad ;			           	
+ 		    this.ciudadesTotales[index] = añadirCiudad ;                 	                
+		    this.mayorPerdida.add(añadirCiudad.nombre()) ;                              	
+		    this.mayorGanancia.add(añadirCiudad.nombre()) ;                             	
+		    index ++ ;
+
         } 
         
         index = 0 ;                                                                       
-	    while ( index < traslados.length ){
-		Traslado trasladoEnCuestion = traslados[ index ] ;                      	   	
-		Traslado añadirTranslados = new Traslado ( trasladoEnCuestion.id , trasladoEnCuestion.origen , trasladoEnCuestion.destino , trasladoEnCuestion.gananciaNeta,trasladoEnCuestion.timestamp);
-		//this.trasladosPorGanancia.add( añadirTranslados ) ;                         	
-		//this.trasladosPorAntiguedad.add( añadirTranslados ) ;	                   	
-		index ++ ;   }                                                              	
+	    while ( index < traslados.length ) {
 
-	    this.totalDespachados = 0 ;						           
+		    Traslado trasladoEnCuestion = traslados[index] ;                      	   	
+		    Traslado añadirTranslados = new Traslado (trasladoEnCuestion) ;
+		    trasladosPorGananciaAux[index] = añadirTranslados ;                         	
+		    trasladosPorAntiguedadAux[index] = añadirTranslados ;	                   	
+		    index ++ ;   
+    
+        }                                                              	
 
-	    //this.mayorSuperavit.FloydAlgorithm();						   
-	    //this.trasladosPorGanancia.FloydAlgorithm() ;					   
-	    //this.trasladosPorAntiguedad.FloydAlgorithm();					   		
-*/
+	    this.totalDespachados = 0 ;
+        this.sumaDeGananciaDeDespachos = 0 ;
+
+        // Transformamos a Heaps las variables auxiliares (definiendo los comparadores) que corresponden a los atributos Heaps.
+
+        Comparator<Ciudad> comparador1 = Comparator.comparing (Ciudad :: superavit) ;
+        comparador1 = comparador1.thenComparing (Ciudad :: nombre) ; 
+        Heaps<Ciudad> mayorSuperavit = new Heaps<Ciudad> (comparador1 , mayorSuperavitAux) ;
+        this.mayorSuperavit = mayorSuperavit ;
+
+        Comparator<Traslado> comparador2 = Comparator.comparing (Traslado :: gananciaNeta) ;
+        comparador2 = comparador2.thenComparing (Traslado :: id) ; 
+        Heaps<Traslado> trasladosPorGanancia = new Heaps<Traslado> (comparador2 , trasladosPorGananciaAux) ;
+        this.trasladosPorGanancia = trasladosPorGanancia ;
+
+        Comparator<Traslado> comparador3 = Comparator.comparing (Traslado :: timestamp) ;
+        comparador2 = comparador3.thenComparing (Traslado :: id) ; 
+        Heaps<Traslado> trasladosPorAntiguedad = new Heaps<Traslado> (comparador3 , trasladosPorAntiguedadAux) ;
+        this.trasladosPorAntiguedad = trasladosPorAntiguedad ;						   					   					   		
+
     }
 
     public void registrarTraslados(Traslado[] traslados){
-        // Implementar
+        
+        int index = 0 ;
+        while (index < traslados.length) {
+
+            // Fabrico el traslado.
+            Traslado añadirTraslado = new Traslado (traslados[index]) ;
+
+            // Lo enchufo en los Heaps.
+            this.trasladosPorGanancia.encolar(añadirTraslado) ;
+            this.trasladosPorAntiguedad.encolar(añadirTraslado) ;
+
+            index ++ ;
+
+        }
     }
 
     public int[] despacharMasRedituables(int n){
