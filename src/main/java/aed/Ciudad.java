962f36a06132416de9e35e252653157f0a62d536
misProyectosPropios <inakifrutos@outlook.com>
2024-11-11 19:37:08 +0100

    FIX problem in Ciudad

--- src/main/java/aed/Ciudad.java
+++ src/main/java/aed/Ciudad.java
@@ -1,7 +1,6 @@
 package aed;
 
 public class Ciudad<T> {
-<<<<<<< HEAD
     int nombre;
     int gananciaNeta;
     int perdidaNeta;
@@ -31,59 +30,6 @@ public class Ciudad<T> {
     }
     public int superavit(){
         return this.superavit;
-=======
-
-    // Atributos privados de la clase.
-    private int nombre ;
-    private int gananciaNeta ;
-    private int perdidaNeta ;
-    private int superavit ;
-
-    // Constructor.
-    public Ciudad (int nombre_index) {
-
-        this.nombre = nombre_index ;
-        this.gananciaNeta = 0 ;
-        this.perdidaNeta = 0 ;
-        this.superavit = gananciaNeta - perdidaNeta ;
-
-    }
-
-    // Constructor por copia.
-    public Ciudad (Ciudad copiado) {
-
-        this.nombre = copiado.nombre ;
-        this.gananciaNeta = copiado.gananciaNeta ;
-        this.perdidaNeta = copiado.perdidaNeta ;
-        this.superavit = copiado.superavit ;
-        
-    }
-
-    public void aumentarPerdida (int monto) {
-
-        this.perdidaNeta += monto ;
-        this.superavit = gananciaNeta - perdidaNeta ;
-
-    }
-    
-    public void aumentarGanancia (int monto) {
-
-        this.gananciaNeta += monto ;
-        this.superavit = gananciaNeta - perdidaNeta ;
-
-    }
-
-    public int obtenerGananciaNeta() {
-
-        return this.gananciaNeta ;
-
-    }
-
-    public int obtenerSuperavit() {
-
-        return this.superavit ;
-
->>>>>>> main
     }
 
 }
