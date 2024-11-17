e7c11270e8b67042afc6e9523e4a668af4e6d374
misProyectosPropios <inakifrutos@outlook.com>
2024-11-17 03:01:28 +0100

    FINISH time

--- src/main/java/aed/BestEffort.java
+++ src/main/java/aed/BestEffort.java
@@ -13,126 +13,132 @@ public class BestEffort {
 
     // O(|C| + |T |)
     public BestEffort (int cantCiudades, Traslado[] traslados){
-        mayorSuperavit = new HeapCiudad(cantCiudades);
+        mayorSuperavit = new HeapCiudad(cantCiudades);                                  // O (|C|)
 
         //Evitar aliasing con traslados
-        Traslado[] arrayCopiaTraslados = new Traslado[traslados.length];
+        Traslado[] arrayCopiaTraslados = new Traslado[traslados.length];                // O(1)
 
-        for(int i = 0; i < arrayCopiaTraslados.length; i++) {
-            arrayCopiaTraslados[i] = new Traslado(traslados[i]);
+        for(int i = 0; i < arrayCopiaTraslados.length; i++) {                           // |Traslados| iteraciones
+            arrayCopiaTraslados[i] = new Traslado(traslados[i]);                        // O(1)
         }
+        //                                                                              Este ciclo pertenece a O(|Traslado|)
+        
+        mayorGanancia = new ArrayList<>(cantCiudades);                                  // O(1)                 
+        mayorPerdida = new ArrayList<>(cantCiudades);                                   // O(1)
 
-        mayorGanancia = new ArrayList<>(cantCiudades);
-        mayorPerdida = new ArrayList<>(cantCiudades);
-
-        for(int i = 0; i < cantCiudades; i++) {
-            mayorGanancia.add(i);
-            mayorPerdida.add(i);
-        }
-
-        this.traslados = new HeapsTraslado(traslados);
+        for(int i = 0; i < cantCiudades; i++) {                                         // O(|Ciudades|)
+            mayorGanancia.add(i);                                                       // O(1)
+            mayorPerdida.add(i);                                                        // O(1)
+        }   
+        //                                                                               Este ciclo pertenece a O(|Ciudades|)      
+        this.traslados = new HeapsTraslado(traslados);                                  // O(|Traslados|)
     }
 
     // O(|traslados| log(|T |))
     public void registrarTraslados(Traslado[] traslados){
         //Evitamso el aliasing
-        Traslado[] arrayCopiaTraslados = new Traslado[traslados.length];
-        for(int i = 0; i < arrayCopiaTraslados.length; i++) {
-            arrayCopiaTraslados[i] = new Traslado(traslados[i]);
-        }
+        Traslado[] arrayCopiaTraslados = new Traslado[traslados.length];            // O(1)
+        for(int i = 0; i < arrayCopiaTraslados.length; i++) {                       // |T| iteraciones
+            arrayCopiaTraslados[i] = new Traslado(traslados[i]);                    // O(1)
+        }   
+        //                                                                         Este ciclo pertenece a O(|T|)
 
-        for(Traslado traslado : arrayCopiaTraslados) {
-            this.traslados.encolar(traslado);
+        for(Traslado traslado : arrayCopiaTraslados) {                              // |T| iteraciones
+            this.traslados.encolar(traslado);                                       // O(1)
         }
+        //                                                                         Este ciclo pertenece a O(|T|)
     }
 
     // O(n (log(|T |) + log(|C|)))
     public int[] despacharMasRedituables(int n){
-        int[] res;
-        if (n > traslados.length()) {
-            res = new int[traslados.length()];
-        } else {
-            res = new int[n];
+        int[] res;                                                                  // O(1)
+        if (n > traslados.length()) {                                               // O(1)
+            res = new int[traslados.length()];                                      // O(1)
+        } else {    
+            res = new int[n];                                                       // O(1)
         }
+        // O(max(O(1), O(1))) = O(1)
 
-        int index = 0;
-        while (n > 0 && traslados.length() > 0) {
+        int index = 0;                                                              // O(1)
+        while (n > 0 && traslados.length() > 0) {                                   // O(n)
 
-            Traslado despachado = traslados.desencolarPorGanancia();
-            res[index] = despachado.id();
-            int gananciaMaxima = mayorSuperavit.getGanancia(mayorGanancia.get(0));
-            int perdidaMaxima = mayorSuperavit.getPerdida(mayorPerdida.get(0));
+            Traslado despachado = traslados.desencolarPorGanancia();                // O(log |T|)
+            res[index] = despachado.id();                                           // O(1)
+            int gananciaMaxima = mayorSuperavit.getGanancia(mayorGanancia.get(0));  // O(1)
+            int perdidaMaxima = mayorSuperavit.getPerdida(mayorPerdida.get(0));     // O(1)
 
-            sumaDeGananciaDeDespachos += despachado.gananciaNeta();
-            totalDespachados++;
+            sumaDeGananciaDeDespachos += despachado.gananciaNeta();                 // O(1)
+            totalDespachados++;                                                     // O(1)
 
-            mayorSuperavit.despacharTraslados(despachado);
+            mayorSuperavit.despacharTraslados(despachado);                          // O(1)
 
             //Modifico el de mayorGanancia si cambia
-            int otraGanancia = mayorSuperavit.getGanancia(despachado.obtenerCiudadOrigen());
-            if (otraGanancia > gananciaMaxima) {
-                mayorGanancia = new ArrayList<>(1);
-                mayorGanancia.add(despachado.obtenerCiudadOrigen());
+            int otraGanancia = mayorSuperavit.getGanancia(despachado.obtenerCiudadOrigen());  // O(1)
+            if (otraGanancia > gananciaMaxima) {                                              // O(1)
+                mayorGanancia = new ArrayList<>(1);                                           // O(1)
+                mayorGanancia.add(despachado.obtenerCiudadOrigen());                          // O(1)
             }
-            else if (otraGanancia == gananciaMaxima) {
-                mayorGanancia.add(despachado.obtenerCiudadOrigen());
+            else if (otraGanancia == gananciaMaxima) {                                        // O(1)
+                mayorGanancia.add(despachado.obtenerCiudadOrigen());                          // O(1)
             }
+            // O(max(O(1), O(1), O(1))) = O(1)
 
-            int otraPerdida = mayorSuperavit.getPerdida(despachado.obtenerCiudadDestino());
+            int otraPerdida = mayorSuperavit.getPerdida(despachado.obtenerCiudadDestino());   // O(1)
 
-            if (otraPerdida > perdidaMaxima) {
-                mayorPerdida = new ArrayList<>(1);
-                mayorPerdida.add(despachado.obtenerCiudadDestino());
+            if (otraPerdida > perdidaMaxima) {                                                // O(1)
+                mayorPerdida = new ArrayList<>(1);                                            // O(1)
+                mayorPerdida.add(despachado.obtenerCiudadDestino());                          // O(1)
             }
-            else if (otraPerdida == perdidaMaxima) {
-                mayorPerdida.add(despachado.obtenerCiudadDestino());
+            else if (otraPerdida == perdidaMaxima) {                                          // O(1)
+                mayorPerdida.add(despachado.obtenerCiudadDestino());                          // O(1)
             }
-            n--;
-            index++;
+            // O(max(O(1),O(1))) = O(1)
+            n--;                                                                              // O(1)
+            index++;                                                                          // O(1)
         }
-        return res;
+        return res;                                                                           // O(1)
     }
 
     // O(n (log(|T |) + log(|C|)))
+    //Este codigo es igual al otro, solo que utilizamos otras variables
     public int[] despacharMasAntiguos(int n){
-        // Implementar
-        int[] res;
-        if (n > traslados.length()) {
-            res = new int[traslados.length()];
+        int[] res;                                                                          // O(1)
+        if (n > traslados.length()) {                                                       // O(1)
+            res = new int[traslados.length()];                                              // O(1)
         } else {
-            res = new int[n];
+            res = new int[n];                                                               // O(1)
         }
 
         int index = 0;
         while (n > 0 && traslados.length() > 0) {
-            Traslado despachado = traslados.desencolarPorTiempo();
-            res[index] = despachado.id();
-            int gananciaMaxima = mayorSuperavit.getGanancia(mayorGanancia.get(0));
-            int perdidaMaxima = mayorSuperavit.getPerdida(mayorPerdida.get(0));
-            sumaDeGananciaDeDespachos += despachado.gananciaNeta();
-            totalDespachados++;
+            Traslado despachado = traslados.desencolarPorTiempo();                                  // O(log n)
+            res[index] = despachado.id();                                                           // O(1)
+            int gananciaMaxima = mayorSuperavit.getGanancia(mayorGanancia.get(0));                  // O(1)
+            int perdidaMaxima = mayorSuperavit.getPerdida(mayorPerdida.get(0));                     // O(1)
+            sumaDeGananciaDeDespachos += despachado.gananciaNeta();                                 // O(1)
+            totalDespachados++;                                                                     // O(1)
 
-            mayorSuperavit.despacharTraslados(despachado);
+            mayorSuperavit.despacharTraslados(despachado);                                          // O(log n)
 
             //Modifico el de mayorGanancia si cambia
-            int otraGanancia = mayorSuperavit.getGanancia(despachado.obtenerCiudadOrigen());
+            int otraGanancia = mayorSuperavit.getGanancia(despachado.obtenerCiudadOrigen());        // O(1)
 
-            if (otraGanancia > gananciaMaxima) {
-                mayorGanancia = new ArrayList<>(1);
-                mayorGanancia.add(despachado.obtenerCiudadOrigen());
+            if (otraGanancia > gananciaMaxima) {                                                    // O(1)
+                mayorGanancia = new ArrayList<>(1);                                                 // O(1)
+                mayorGanancia.add(despachado.obtenerCiudadOrigen());                                // O(1) amortizado
             }
-            else if (otraGanancia == gananciaMaxima) {
-                mayorGanancia.add(despachado.obtenerCiudadOrigen());
+            else if (otraGanancia == gananciaMaxima) {                                              // O(1)
+                mayorGanancia.add(despachado.obtenerCiudadOrigen());                                // O(1) amortizado
             }
 
-            int otraPerdida = mayorSuperavit.getPerdida(despachado.obtenerCiudadDestino());
+            int otraPerdida = mayorSuperavit.getPerdida(despachado.obtenerCiudadDestino());         // O(1)
 
-            if (otraPerdida > perdidaMaxima) {
-                mayorPerdida = new ArrayList<>(1);
-                mayorPerdida.add(despachado.obtenerCiudadDestino());
+            if (otraPerdida > perdidaMaxima) {                                                      // O(1)
+                mayorPerdida = new ArrayList<>(1);                                                  // O(1)
+                mayorPerdida.add(despachado.obtenerCiudadDestino());                                // O(1) amortizado
             }
             else if (otraPerdida == perdidaMaxima) {
-                mayorPerdida.add(despachado.obtenerCiudadDestino());
+                mayorPerdida.add(despachado.obtenerCiudadDestino());                                // O(1) amortizado
             }
             index++;                                                                                 // O(1)
             n--;                                                                                     // O(1)
